a432dba017c27500b2aecb50319df288a57dfcca
kelvinlocc <ccloadust@gmail.com>
2017-10-26 17:40:43 +0800

    menu bar
    fixing menu bar

--- app/home/home.js
+++ app/home/home.js
@@ -11,65 +11,46 @@ angular.module('myApp.home', ['ngRoute'])
     }])
 
     // Home controller
-    .controller('HomeCtrl', ['$scope','$firebase','$firebaseArray',function($scope,$firebas,$firebaseArray) {
-        // var domain = "https://online-store-uat.firebaseapp.com";
+    .controller('HomeCtrl', ['$scope', '$firebase', '$firebaseArray', '$firebaseAuth', '$rootScope', function ($scope, $firebas, $firebaseArray, $firebaseAuth, $rootScope) {
         var rootRef = firebase.database().ref();
+        var auth = $firebaseAuth();
+        // var ref = new Firebase('https://online-store-uat.firebaseio.com');
+        $scope.user = {};
+        // $scope.test = "test";
+        $scope.user.email = "test123@gmail.com";
+        $scope.user.password = "test123";
+
+
+        $scope.SignIn = function (event) {
+            // var username = $scope.email;
+            var username = $scope.user.email;
+            var password = $scope.user.password;
+            console.log(" e p " + username + ", " + "");
+
+
+            firebase.auth().signInWithEmailAndPassword(username, password)
+                .catch(function (error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    if (errorCode === 'auth/wrong-password') {
+                        alert('Wrong password.');
+                    } else {
+                        alert(errorMessage);
+                    }
+                    console.log(error);
+                });
+        }
+
         var temp = firebase.database().ref().child("users");
+
 
         temp.on('child_added', function (snap) {
             var myObj = snap.val();
             // console.log(myObj);
             console.log(myObj.name);
             console.log(myObj.email);
-            // console.log("hi ", snap.val());
+
         });
-
-        //
-        // chatsRef.orderByValue('users').equalTo(‘user1’).once(‘value’, function (snap) {
-        //     var user1Chats = snap.val();
-        //     console.log(‘user1 chats!’, user1Chats);
-        // });
-
-
-        $scope.userdata = $firebaseArray(temp);
-        $scope.userdata.$ref().once('value', function(snap) {
-          console.log("snap.val() ",snap.val())
-        });
-
-
-        $scope.SignIn = function(event) {
-            ref.authWithOAuthPopup("facebook", function(error, authData) {
-            });
-
-            ref.auth().signInWithEmailAndPassword("test123@gmail.com", "test123").catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                $("#loginError").show().text(errorMessage);
-                $("#loginProgress").hide();
-                $("#loginBtn").show();
-                console.log("error "+errorCode+", "+errorMessage);
-
-
-
-            });
-
-            // event.preventDefault();  // To prevent form refresh
-            // var username = $scope.user.email;
-            // var password = $scope.user.password;
-            //
-            // loginObj.$login('password', {
-            //     email: username,
-            //     password: password
-            // })
-            //     .then(function(user) {
-            //         // Success callback
-            //         console.log('Authentication successful');
-            //     }, function(error) {
-            //         // Failure callback
-            //         console.log('Authentication failure');
-            //         console.log("error "+error);
-            //     });
-        }
 
     }]);
